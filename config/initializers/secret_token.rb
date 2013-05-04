8f1b52ca28ca87b2f4a6c403f4443177c81c5f0c
Brett Camarda <bcamarda@gmail.com>
2013-05-04 15:56:07 -0700

    initial commit with psql and no test suite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootBook::Application.config.secret_token = 'cdc06b35e51b81a06ee6e105497c7c22d6b4f0445a389966456209ee0bca52c92366ad52bf6a975457ff42f9401bd3b6a9a9069ac6dcbafb76212d93aece972c'
