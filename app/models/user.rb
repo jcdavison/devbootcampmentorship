a3af1e4a8a7514adb62373e38ea931e032b88754
Brett Camarda <bcamarda@gmail.com>
2013-05-15 20:32:02 -0700

    validating uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :pic, :role
   validates_presence_of :email, :first_name, :last_name
+  validates_uniqueness_of :email
 
   has_many :authorizations, :dependent => :destroy
   belongs_to :cohort
