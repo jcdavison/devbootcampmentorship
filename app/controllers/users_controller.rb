e6676661814c2232be30ffeb8ec69eae4aff91da
Brett Camarda <bcamarda@gmail.com>
2013-06-16 12:47:35 -0700

    forked the wufoo gem to return the results we need, updated our rake task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,23 @@ class UsersController < ApplicationController
 
   def create
     if @user = User.find_by_email(auth_hash[:email]) && @authorization = Authorization.find_by_uid(auth_hash[:uid])
-      session[:current_user_id] = @authorization.user.id 
+      session[:current_user_id] = @authorization.user.id
       route_user(@user)
     elsif @user = User.find_by_email(auth_hash[:email])
-      @user.new_auth(auth_hash) 
+      @user.new_auth(auth_hash)
       route_user(@user)
       # redirect_to edit_mentor_path @user if @user.avail_mentor? && @user.mentees.empty?
       # redirect_to edit_boot_path @user if ! @user.avail_mentor?
     elsif
       @user = User.new
       @user.set_attributes(auth_hash, params)
-      session[:current_user_id] = @user.id if @user.save 
+      session[:current_user_id] = @user.id if @user.save
       @user.new_auth(auth_hash)
       route_user(@user)
       # redirect_to edit_mentor_path @user if @user.avail_mentor? && @user.mentees.empty?
       # redirect_to edit_boot_path @user if ! @user.avail_mentor?
       # redirect_to cohort_path current_user.cohort.id if @user.mentor? == "busy"
-      # redirect_to cohort_path current_user.cohort.id 
+      # redirect_to cohort_path current_user.cohort.id
     elsif
       redirect_to sign_in_path
     end
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   def update
     User.find(params[:id]).update_attributes(params[:user])
-    redirect_to cohorts_path 
+    redirect_to cohorts_path
   end
   protected
 
--- lib/tasks/fuck_wufoo.rake
+++ lib/tasks/fuck_wufoo.rake
@@ -2,10 +2,11 @@ ACCOUNT = 'devbootcamp'
 API_KEY = 'X1HQ-ZU20-12O7-0SD9'
 FORM = "dev-bootcamp-mentor-application"
 namespace :wu do
-  task :run  => :environment do 
+  task :run  => :environment do
     wufoo = WuParty.new(ACCOUNT, API_KEY)
     form = wufoo.form(FORM)
-    form.entries.each { |user| User.process_wufoo_user(user)}
+    all_entries = form.all_entries
+    all_entries.each { |user| User.process_wufoo_user(user)}
   end
 
 end
