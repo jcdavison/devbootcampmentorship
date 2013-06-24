e7623b4a80a438d8b898811dc79c2b92081568d3
Brett Camarda <bcamarda@gmail.com>
2013-06-23 23:38:58 -0700

    ascii problem

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ActionMailer::Base
   default from: "admin@devbootcampmentorship.com"
 
   def test_email
-    mail(:to => “brett.coding@gmail.com”, :subject => “Test Email!”)
+    mail(to: "brett.coding@gmail.com", subject: "Test Email!")
   end
 end
