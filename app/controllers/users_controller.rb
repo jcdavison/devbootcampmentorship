d057da55301d628da5c82a2810ab6a9d150a2606
John Davison <johncdavison@gmail.com>
2013-06-26 23:35:21 -0700

    Add location select to user sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @locations = [ "San Francisco", "Chicago"]
   end
 
   def index
   end
 
   def create
+    binding.pry
     @user = User.new(params[:user])
+    @user.location = params[:location]
     if @user.save
+
       redirect_to thank_you_path(id: @user.id)
     else
       redirect_to :back
