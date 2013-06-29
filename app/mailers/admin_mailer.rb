7d2b6295f328c02e5145e2f9af38f6ba7b9ff434
John Davison <johncdavison@gmail.com>
2013-06-29 12:01:56 -0700

    Add notify all function to cohort.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,4 +4,9 @@ class AdminMailer < ActionMailer::Base
   def test_email
     mail(to: "brett.coding@gmail.com", subject: "Test Email!")
   end
+
+  def notify_pair(to, content)
+    @content = content
+    mail(to: to, subject: "test email", body: @content)
+  end
 end
