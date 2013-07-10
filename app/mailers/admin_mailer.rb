eaa0fc25cf775b9e68c771e6fe68f98e3e3b6bcd
John Davison <johncdavison@gmail.com>
2013-07-10 09:20:19 -0700

    notify welcome

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,7 @@
 class AdminMailer < ActionMailer::Base
   default from: "admin@devbootcampmentorship.com"
   layout "mailer"
+  CC_LINE = "john@devbootcamp.com, brett@devbootcamp.com"
 
   def test_email
     mail(to: "brett.coding@gmail.com", subject: "Test Email!")
@@ -8,9 +9,9 @@ class AdminMailer < ActionMailer::Base
 
   def welcome(user)
     @user = user
-    return unless @user
     @cohort = Cohort.next
-    mail(to: @user.email, subject: "Welcome to the Dev Bootcamp Mentorship community", cc: "john@devbootcamp.com")
+    return unless @user && @cohort
+    mail(to: @user.email, subject: "Welcome to the Dev Bootcamp Mentorship community", cc: CC_LINE )
   end
 
   def notify_pair(mentor, mentee, cohort_name)
@@ -18,7 +19,7 @@ class AdminMailer < ActionMailer::Base
     @mentor = mentor
     @mentee = mentee
     to = "#{@mentor.email}, #{@mentee.email}"
-    mail(to: to, subject: "DevBootcamp Mentor Pairing re #{cohort_name}!", cc: "john@devbootcamp.com, brett@devbootcamp.com")
+    mail(to: to, subject: "DevBootcamp Mentor Pairing re #{cohort_name}!", cc: CC_LINE)
   end
 
   def notify_pair_destruction(mentor, mentee)
@@ -26,6 +27,6 @@ class AdminMailer < ActionMailer::Base
     @mentor = mentor
     @mentee = mentee
     to = "#{@mentor.email}, #{@mentee.email}"
-    mail(to: to, subject: "DevBootcamp Mentor Pairing, Its a Break Up..", cc: "john@devbootcamp.com, brett@devbootcamp.com")
+    mail(to: to, subject: "DevBootcamp Mentor Pairing, Its a Break Up..", cc: CC_LINE)
   end
 end
