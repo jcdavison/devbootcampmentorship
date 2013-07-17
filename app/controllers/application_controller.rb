a21ae79e0e95cc61b2b915c70268ad00f5af159e
John Davison <johncdavison@gmail.com>
2013-07-17 06:49:29 -0700

    set admin permissions to on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def logged_in?
-    true
-    # session[:current_user_id] != nil ? true : false  
+    session[:current_user_id] != nil ? true : false  
   end
 
   def current_user
@@ -20,13 +19,13 @@ class ApplicationController < ActionController::Base
   end
 
   def is_admin?
-    # current_user && current_user.admin? ? true : false
+    current_user && current_user.admin? ? true : false
   end
 
   def protect_admin
-    # unless is_admin?
-    #   redirect_to root_path
-    # end
+    unless is_admin?
+      redirect_to root_path
+    end
   end
 
   helper_method :logged_in?, :current_user, :admin?, :protect_admin
